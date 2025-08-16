1bba50a4b210412d934ac345adf0ed7baf88c826
gabrielemasciulli <gmasciu@gmail.com>
2025-08-16 19:22:23 +0200

    New solution

---
+++ NeetCode-150-Solutions/28_binary-search.py
@@ -0,0 +1,36 @@
+class Solution:
+    """
+    You are given an array of distinct integers nums, sorted in ascending order, and an integer target.
+
+    Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
+
+    Your solution must run in O(logn) time.
+
+    Example 1:
+
+    Input: nums = [-1,0,2,4,6,8], target = 4
+
+    Output: 3
+    Example 2:
+
+    Input: nums = [-1,0,2,4,6,8], target = 3
+
+    Output: -1
+    """
+
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        l = 0
+        r = n - 1
+
+        while l <= r:
+            m = l + (r - l) // 2
+            num = nums[m]
+
+            if num == target:
+                return m
+            elif target < num:
+                r = m - 1
+            else:
+                l = m + 1
+        return -1
